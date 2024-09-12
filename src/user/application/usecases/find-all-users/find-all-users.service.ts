a558636ce9179f6684a50e34ca90cb58c0365ff9
Cassio <cassiojr0108@gmail.com>
2024-09-12 15:40:44 -0300

    FindAllUsers UseCase

---
+++ src/user/application/usecases/find-all-users/find-all-users.service.ts
@@ -0,0 +1,19 @@
+import { UseCase as DefaultUseCase } from '@/shared/usecases/use-case'
+import { UserRepository } from '@/user/infrastructure/database/repository/user.repository'
+import { Injectable } from '@nestjs/common'
+import { UserOutput } from '../../dtos/user-output'
+
+export namespace FindAllUsersService {
+  export type Input = null
+
+  export type Output = UserOutput[]
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly userRepository: UserRepository) {}
+
+    async execute(): Promise<Output> {
+      return this.userRepository.findAll()
+    }
+  }
+}
