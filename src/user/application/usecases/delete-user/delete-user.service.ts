79ad5ffe4c194b9c2d1faf28c0a6937fc5182a1f
Cassio <cassiojr0108@gmail.com>
2024-09-12 15:33:49 -0300

    DeleteUser UseCase

---
+++ src/user/application/usecases/delete-user/delete-user.service.ts
@@ -0,0 +1,19 @@
+import { UseCase as DefaultUseCase } from '@/shared/usecases/use-case'
+import { UserRepository } from '@/user/infrastructure/database/repository/user.repository'
+import { Injectable } from '@nestjs/common'
+
+export namespace DeleteUserService {
+  export type Input = {
+    userId: string
+  }
+
+  export type Output = void
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly userRepository: UserRepository) {}
+    async execute(input: Input): Promise<void> {
+      await this.userRepository.delete(input.userId)
+    }
+  }
+}
