44243f369ce96f731828024461bc54beb3d4bb13
Cassio <cassiojr0108@gmail.com>
2024-09-13 15:57:02 -0300

    LoginUser UseCase

--- src/user/application/usecases/login-user/login-user.service.ts
+++ src/user/application/usecases/login-user/login-user.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common'
+import { HashProviderService } from '@/shared/providers/hash-provider/hash-provider.service'
+import { UseCase as DefaultUseCase } from '@/shared/usecases/use-case'
+import { UserRepository } from '@/user/infrastructure/database/repository/user.repository'
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { UserOutput } from '../../dtos/user-output'
 
-@Injectable()
-export class LoginUserService {}
+export namespace LoginUserService {
+  export type Input = {
+    email: string
+    password: string
+  }
+
+  export type Output = UserOutput
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(
+      private userRepository: UserRepository,
+      private hashProvider: HashProviderService,
+    ) {}
+
+    async execute(input: Input) {
+      const { email, password } = input
+
+      if (!email || !password) {
+        throw new BadRequestException('Dados de entrada não fornecidos')
+      }
+
+      const entity = await this.userRepository.findByEmail(email)
+
+      const hashPasswordMatches = await this.hashProvider.compareHash(
+        password,
+        entity.password,
+      )
+
+      if (!hashPasswordMatches) {
+        throw new UnauthorizedException('Credenciais inválidas')
+      }
+
+      return entity
+    }
+  }
+}
