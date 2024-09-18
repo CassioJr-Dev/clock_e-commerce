f29ecd08c3a52c1e065b4b58fa3fc6a57147f28a
Cassio <cassiojr0108@gmail.com>
2024-09-18 17:06:10 -0300

    Update Entity

--- src/product/model/product.entity.ts
+++ src/product/model/product.entity.ts
@@ -1,11 +1,14 @@
 import { Product } from '@prisma/client'
 
-export class ProductEntity implements Omit<Product, 'price'> {
+export class ProductEntity implements Omit<Product, 'price' | 'oldPrice'> {
   productId: string
   name: string
   description: string
   price: number
+  oldPrice: number
   stock: number
+  image: string
+  category: string
   adminId: string
   created_at: Date
 }
