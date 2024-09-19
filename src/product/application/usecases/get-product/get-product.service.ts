1756c49ad873d18110a760e59d24c66e0599c4f0
Cassio <cassiojr0108@gmail.com>
2024-09-19 16:04:39 -0300

    GetProduct UseCase

--- src/product/application/usecases/get-product/get-product.service.ts
+++ src/product/application/usecases/get-product/get-product.service.ts
@@ -1,14 +1,21 @@
+import { ProductRepository } from '@/product/infrastructure/database/repository/product.repository'
 import { UseCase as DefaultUseCase } from '@/shared/usecases/use-case'
 import { Injectable } from '@nestjs/common'
+import { ProductOutput } from '../../dtos/product-output.dto'
 
 export namespace GetProductService {
-  export type Input = {}
-  export type Output = {}
+  export type Input = {
+    productId: string
+    adminId: string
+  }
+  export type Output = ProductOutput
 
   @Injectable()
   export class UseCase implements DefaultUseCase<Input, Output> {
-    execute(input: Input): Output | Promise<Output> {
-      throw new Error('Method not implemented.')
+    constructor(private readonly productRepository: ProductRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      return this.productRepository.findById(input.productId)
     }
   }
 }
