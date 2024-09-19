0f02ae46c8088f78390d3e53d11a8809fd2bb752
Cassio <cassiojr0108@gmail.com>
2024-09-19 16:32:52 -0300

    SearchProduct UseCase

--- src/product/application/usecases/search-product/search-product.service.ts
+++ src/product/application/usecases/search-product/search-product.service.ts
@@ -1,14 +1,22 @@
+import { ProductRepository } from '@/product/infrastructure/database/repository/product.repository'
+import { PaginationOutput } from '@/shared/application/dtos/pagination-output.dto'
+import { SearchInput } from '@/shared/application/dtos/search-input.dto'
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { SearchParams } from '@/shared/database/repositories/searchable.repository-contracts'
 import { Injectable } from '@nestjs/common'
 
 export namespace SearchProductService {
-  export type Input = {}
-  export type Output = {}
+  export type Input = SearchInput
+  export type Output = PaginationOutput
 
   @Injectable()
   export class UseCase implements DefaultUseCase<Input, Output> {
-    execute(input: Input): Output | Promise<Output> {
-      throw new Error('Method not implemented.')
+    constructor(private readonly productRepository: ProductRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const params = new SearchParams(input)
+      const searchResult = await this.productRepository.search(params)
+      return searchResult
     }
   }
 }
