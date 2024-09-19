246a62ac759eb86563f804022faa8fff03ebe0a5
Cassio <cassiojr0108@gmail.com>
2024-09-19 16:16:17 -0300

    UpdateProduct UseCase

--- src/product/application/usecases/update-product/update-product.service.ts
+++ src/product/application/usecases/update-product/update-product.service.ts
@@ -1,14 +1,40 @@
+import { ProductRepository } from '@/product/infrastructure/database/repository/product.repository'
 import { UseCase as DefaultUseCase } from '@/shared/usecases/use-case'
 import { Injectable } from '@nestjs/common'
+import { ProductInput } from '../../dtos/product-input.dto'
 
 export namespace UpdateProductService {
-  export type Input = {}
+  export type Input = ProductInput & { productId: string }
   export type Output = {}
 
   @Injectable()
   export class UseCase implements DefaultUseCase<Input, Output> {
-    execute(input: Input): Output | Promise<Output> {
-      throw new Error('Method not implemented.')
+    constructor(private readonly productRepository: ProductRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const {
+        productId,
+        name,
+        description,
+        price,
+        oldPrice,
+        stock,
+        image,
+        category,
+        adminId,
+      } = input
+
+      const product = await this.productRepository.findById(productId, adminId)
+
+      if (name) product.name = name
+      if (description) product.description = description
+      if (price) product.price = price
+      if (oldPrice) product.oldPrice = oldPrice
+      if (stock) product.stock = stock
+      if (image) product.image = image
+      if (category) product.category = category
+
+      return await this.productRepository.update({ ...product })
     }
   }
 }
