157fd89e0c117e4c1f49ac7437f746fb0f4f6d16
Cassio <cassiojr0108@gmail.com>
2024-09-27 19:37:05 -0300

    CartItem Repository

---
+++ src/cartItem/infrastructure/database/repository/cartItem.repository.ts
@@ -0,0 +1,114 @@
+import { CartEntity } from '@/cart/model/cart.entity'
+import { CartItemEntity } from '@/cartItem/model/cartItem.entity'
+import { PrismaService } from '@/shared/database/database.service'
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+
+@Injectable()
+export class CartItemRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async addItemToCart(
+    item: CartItemEntity,
+    userId: string,
+  ): Promise<CartItemEntity> {
+    const cartExists = await this.cartExists(item.cartId, userId)
+
+    const productExists = await this.prismaService.product.findUnique({
+      where: {
+        productId: item.productId,
+      },
+    })
+
+    if (!productExists) {
+      throw new NotFoundException('Produto não encontrado')
+    }
+
+    if (!cartExists) {
+      throw new NotFoundException('Carrinho não encontrado')
+    }
+
+    const itemExists = await this.prismaService.cartItem.findMany({
+      where: { productId: item.productId, cartId: item.cartId },
+    })
+
+    if (itemExists.length) {
+      throw new ConflictException('O produto já existe no carrinho')
+    }
+
+    const addItem = await this.prismaService.cartItem.create({
+      data: item,
+    })
+
+    return addItem
+  }
+
+  async removeItemFromCart(
+    itemId: string,
+    cartId: string,
+    userId: string,
+  ): Promise<void> {
+    await this.cartExists(cartId, userId)
+
+    await this.itemExists(itemId, cartId)
+
+    await this.prismaService.cartItem.delete({
+      where: { itemId, cartId },
+    })
+  }
+
+  async updateQuantity(
+    itemEntity: CartItemEntity,
+    userId: string,
+  ): Promise<CartItemEntity> {
+    await this.cartExists(itemEntity.cartId, userId)
+
+    const item = await this.itemExists(itemEntity.itemId, itemEntity.cartId)
+
+    const updateItem = await this.prismaService.cartItem.update({
+      where: { itemId: itemEntity.itemId, cartId: itemEntity.cartId },
+      data: itemEntity,
+    })
+
+    return updateItem
+  }
+
+  async findAll(cartId: string, userId: string): Promise<CartItemEntity[]> {
+    await this.cartExists(cartId, userId)
+
+    const cartItems = await this.prismaService.cartItem.findMany({
+      where: {
+        cartId,
+      },
+    })
+
+    return cartItems
+  }
+
+  async itemExists(itemId: string, cartId: string): Promise<CartItemEntity> {
+    const findItem = await this.prismaService.cartItem.findUnique({
+      where: { itemId, cartId },
+    })
+
+    if (!findItem) {
+      throw new NotFoundException('Carrinho não encontrado')
+    }
+
+    return findItem
+  }
+
+  async cartExists(cartId: string, userId: string): Promise<CartEntity> {
+    const findCart = await this.prismaService.cart.findUnique({
+      where: { cartId, userId },
+    })
+
+    if (!findCart) {
+      throw new NotFoundException('Carrinho não encontrado')
+    }
+
+    return findCart
+  }
+}
