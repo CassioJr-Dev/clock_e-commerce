ce5ce88ea8cb63176b7b9584af9da5c1e5ad7f41
Cassio <cassiojr0108@gmail.com>
2024-09-27 21:00:49 -0300

    FindAllItems Service

---
+++ src/cartItem/application/usecases/findAll-cart/findAll.service.ts
@@ -0,0 +1,22 @@
+import { CartItemRepository } from '@/cartItem/infrastructure/database/repository/cartItem.repository'
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { Injectable } from '@nestjs/common'
+
+export namespace FindAllUsersService {
+  export type Input = {
+    cartId: string
+    userId: string
+  }
+
+  export type Output = any
+  // export type Output = (CartItemOutput & ProductOutput)[]
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly cartItemRepository: CartItemRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      return this.cartItemRepository.findAll(input.cartId, input.userId)
+    }
+  }
+}
