bb228e87b0d0cc3cea8629b9da670f36d5482d73
Cassio <cassiojr0108@gmail.com>
2024-09-27 21:01:15 -0300

    GetItem Service

---
+++ src/cartItem/application/usecases/getItem-cart/getItem.service.ts
@@ -0,0 +1,21 @@
+import { CartItemRepository } from '@/cartItem/infrastructure/database/repository/cartItem.repository'
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { Injectable } from '@nestjs/common'
+import { CartItemOutput } from '../../dtos/cartItem-output.dto'
+
+export namespace GetProductService {
+  export type Input = {
+    itemId: string
+    cartId: string
+  }
+  export type Output = CartItemOutput
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly cartItemRepository: CartItemRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      return this.cartItemRepository.findItem(input.itemId, input.cartId)
+    }
+  }
+}
