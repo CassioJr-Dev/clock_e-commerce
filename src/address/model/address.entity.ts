cff113d5bdd275903416d9916a6681078445c96f
Cassio <cassiojr0108@gmail.com>
2024-10-01 15:38:40 -0300

    AddressEntity

---
+++ src/address/model/address.entity.ts
@@ -0,0 +1,15 @@
+import { Address } from '@prisma/client'
+
+export class AddressEntity implements Address {
+  addressId: string
+  cep: string
+  street: string
+  complement: string
+  unit: string
+  neighborhood: string
+  city: string
+  uf: string
+  state: string
+  userId: string
+  created_at: Date
+}
