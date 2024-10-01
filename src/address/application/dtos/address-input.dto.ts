d631ebb82ab9574339dfcdc92f1849326529aca7
Cassio <cassiojr0108@gmail.com>
2024-10-01 16:51:08 -0300

    Dtos of Application

---
+++ src/address/application/dtos/address-input.dto.ts
@@ -0,0 +1,11 @@
+export type AddressInput = {
+  cep: string
+  street: string
+  complement: string
+  unit: string
+  neighborhood: string
+  city: string
+  uf: string
+  state: string
+  userId: string
+}
---
+++ src/address/application/dtos/address-output.dto.ts
@@ -0,0 +1,13 @@
+export type AddressOutput = {
+  addressId: string
+  cep: string
+  street: string
+  complement: string
+  unit: string
+  neighborhood: string
+  city: string
+  uf: string
+  state: string
+  userId: string
+  created_at: Date
+}
