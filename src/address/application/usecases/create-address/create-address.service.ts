c59e20d55a50ed1cb89d8a8f0f26402c99d16599
Cassio <cassiojr0108@gmail.com>
2024-10-01 16:53:26 -0300

    CreateAddress UseCase

---
+++ src/address/application/usecases/create-address/create-address.service.ts
@@ -0,0 +1,36 @@
+import { AddressRepository } from '@/address/infrastructure/database/repository/address.repository'
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { DateProviderService } from '@/shared/providers/date-provider/date-provider.service'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { randomUUID } from 'crypto'
+import { AddressInput } from '../../dtos/address-input.dto'
+import { AddressOutput } from '../../dtos/address-output.dto'
+
+export namespace CreateAddressService {
+  export type Input = AddressInput
+
+  export type Output = AddressOutput
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly addressRepository: AddressRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (Object.values(input).some(value => !value)) {
+        throw new BadRequestException('Todos os campos são obrigatórios.')
+      }
+
+      const addressId = randomUUID()
+
+      const created_at = DateProviderService.toDate()
+
+      const entity = {
+        addressId,
+        ...input,
+        created_at,
+      }
+
+      return await this.addressRepository.insert(entity)
+    }
+  }
+}
