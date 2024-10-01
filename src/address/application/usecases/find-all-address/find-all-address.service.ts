6c3c86fb322641121e2a6c64e4507c01f07924a9
Cassio <cassiojr0108@gmail.com>
2024-10-01 17:03:47 -0300

    FindAllAddress UseCase

---
+++ src/address/application/usecases/find-all-address/find-all-address.service.ts
@@ -0,0 +1,22 @@
+import { AddressRepository } from '@/address/infrastructure/database/repository/address.repository'
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
+import { Injectable } from '@nestjs/common'
+import { AddressOutput } from '../../dtos/address-output.dto'
+
+export namespace FindAllAddressService {
+  export type Input = {
+    cartId: string
+    userId: string
+  }
+
+  export type Output = AddressOutput[]
+
+  @Injectable()
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly addressRepository: AddressRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      return this.addressRepository.findAll(input.userId)
+    }
+  }
+}
