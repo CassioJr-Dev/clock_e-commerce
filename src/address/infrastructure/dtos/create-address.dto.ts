8857be517ffca209b0398127691a886a8da6c8c4
Cassio <cassiojr0108@gmail.com>
2024-10-01 20:42:52 -0300

    Validators Dtos

--- src/address/infrastructure/dtos/create-address.dto.ts
+++ src/address/infrastructure/dtos/create-address.dto.ts
@@ -1,14 +1,37 @@
-import { AddressOutput } from '@/address/application/dtos/address-output.dto'
+import { AddressInput } from '@/address/application/dtos/address-input.dto'
+import { IsNotEmpty, IsPostalCode, IsString } from 'class-validator'
 
-export class CreateAddressDto implements Omit<AddressOutput, 'userId'> {
-  addressId: string
+export class CreateAddressDto implements Omit<AddressInput, 'userId'> {
+  @IsPostalCode('BR', { message: 'Código postal inválido para o Brasil.' })
+  @IsString()
+  @IsNotEmpty()
   cep: string
+
+  @IsString()
+  @IsNotEmpty()
   street: string
+
+  @IsString()
+  @IsNotEmpty()
   complement: string
+
+  @IsString()
+  @IsNotEmpty()
   unit: string
+
+  @IsString()
+  @IsNotEmpty()
   neighborhood: string
+
+  @IsString()
+  @IsNotEmpty()
   city: string
+
+  @IsString()
+  @IsNotEmpty()
   uf: string
+
+  @IsString()
+  @IsNotEmpty()
   state: string
-  created_at: Date
 }
--- src/address/infrastructure/dtos/update-address.dto.ts
+++ src/address/infrastructure/dtos/update-address.dto.ts
@@ -1,14 +1,41 @@
-import { AddressOutput } from '@/address/application/dtos/address-output.dto'
+import { AddressInput } from '@/address/application/dtos/address-input.dto'
+import { IsNotEmpty, IsPostalCode, IsString } from 'class-validator'
 
-export class UpdateAddressDto implements Omit<AddressOutput, 'userId'> {
+export class UpdateAddressDto implements Omit<AddressInput, 'userId'> {
+  @IsString()
+  @IsNotEmpty()
   addressId: string
+
+  @IsPostalCode('BR', { message: 'Código postal inválido para o Brasil.' })
+  @IsString()
+  @IsNotEmpty()
   cep: string
+
+  @IsString()
+  @IsNotEmpty()
   street: string
+
+  @IsString()
+  @IsNotEmpty()
   complement: string
+
+  @IsString()
+  @IsNotEmpty()
   unit: string
+
+  @IsString()
+  @IsNotEmpty()
   neighborhood: string
+
+  @IsString()
+  @IsNotEmpty()
   city: string
+
+  @IsString()
+  @IsNotEmpty()
   uf: string
+
+  @IsString()
+  @IsNotEmpty()
   state: string
-  created_at: Date
 }
